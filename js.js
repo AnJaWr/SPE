aeaf40e97e1884e9e5c6cac5afef6f3dac712ac6
anjawr <48285666+AnJaWr@users.noreply.github.com>
2025-09-18 22:35:06 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,7 @@
+ document.getElementById("egzamin").addEventListener("click", function() {
+    window.location.hash = "egzamin-sekcja";})
+    
+
+        function toggleMenu() {
+      document.querySelector("nav ul").classList.toggle("active");
+    }
